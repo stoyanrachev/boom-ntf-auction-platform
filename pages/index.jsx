56755938be1360cf9b469b354e65a6ec204aae4c
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-02-11 19:58:26 +0200

    User Component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,19 @@
 import Header from "../src/components/header/Header";
 import Avatar from "../src/components/avatar/Avatar";
+import User from "../src/components/user/User";
 
 export default function Index() {
   return (
     <>
       <Header />
-      <Avatar verified='true' url='/images/avatar.png'/>
+      <Avatar verified="true" size="50px" url="/images/avatar.png" />
+      <User
+        name="Elsa"
+        info="Im Miner"
+        avatar="/images/avatar.png"
+        size="53px"
+        verified={true}
+      />
     </>
   );
 }
