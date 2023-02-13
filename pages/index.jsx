9763e14a4c5d6ad8f3b2c95681e493aa9d295c9e
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-02-13 21:32:01 +0200

    Featured Component

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,11 +6,62 @@ import Trending from "../src/components/trending/Trending";
 import Auctions from "../src/components/auctions/Auctions";
 import Footer from "../src/components/footer/Footer";
 import How from "../src/components/how/How";
+import Featured from "../src/components/featured/Featured";
 
 export default function Index() {
   return (
     <>
       <Header />
+
+      <Featured
+        items={[
+          {
+            image:
+              "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=500&h=500",
+            title: "Breakfast",
+            rows: 2,
+            cols: 3,
+            href: "/about",
+          },
+          {
+            image:
+              "https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=500&h=500",
+            title: "Burger",
+            href: "/about",
+          },
+          {
+            image:
+              "https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=500&h=500",
+            title: "Camera",
+            href: "/about",
+          },
+          {
+            image:
+              "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=500&h=500",
+            title: "Coffee",
+            href: "/about",
+          },
+          {
+            image:
+              "https://images.unsplash.com/photo-1533827432537-70133748f5c8?w=500&h=500",
+            title: "Hats",
+            href: "/about",
+          },
+          {
+            image:
+              "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=500&h=500",
+            title: "Honey",
+            href: "/about",
+          },
+          {
+            image:
+              "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=500&h=500",
+            title: "Basketball",
+            href: "/about",
+          },
+        ]}
+      />
+
       <Avatar verified="true" size="50px" url="/images/avatar.png" />
       <User
         name="Elsa"
@@ -28,15 +79,18 @@ export default function Index() {
         items={[
           {
             title: "Digital Currency",
-            description: "You can get ETH, the digital currency that fuels transactions on the Ethereum blockchain, from a digital currency exchange",
+            description:
+              "You can get ETH, the digital currency that fuels transactions on the Ethereum blockchain, from a digital currency exchange",
           },
           {
             title: "Crypto Wallet",
-            description: "A crypto wallet, such as MetaMask, stores your ETH and processes transactions on the Ethereum blockchain.",
+            description:
+              "A crypto wallet, such as MetaMask, stores your ETH and processes transactions on the Ethereum blockchain.",
           },
           {
             title: "BUM.",
-            description: "Let's connect your wallet to BUM, edit your profile, and begin interacting in the space. ",
+            description:
+              "Let's connect your wallet to BUM, edit your profile, and begin interacting in the space. ",
           },
         ]}
         link="https://app.boom.dev"
