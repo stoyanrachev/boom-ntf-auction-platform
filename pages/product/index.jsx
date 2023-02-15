662d50242dbd7c63730ea662b65638eb5a00a960
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-02-15 14:09:58 +0200

    Product Info Title

--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -1,12 +1,14 @@
 import Header from "../../src/components/header/Header";
 import Footer from "../../src/components/footer/Footer";
 import ProductImage from "../../src/components/product/ProductImage";
+import ProductInfoTitle from "../../src/components/product/ProductInfoTitle";
 
 export default function Product() {
   return (
     <>
       <Header />
       <ProductImage url="/images/nft.jpg" />
+      <ProductInfoTitle text="Shallow Son" />
       <Footer />
     </>
   );
