ce44b02e4db5acad9355f78bb7b9c118da3305f9
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-02-15 20:12:34 +0200

    Product Info Price

--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -2,6 +2,7 @@ import Header from "../../src/components/header/Header";
 import Footer from "../../src/components/footer/Footer";
 import ProductImage from "../../src/components/product/ProductImage";
 import ProductInfoTitle from "../../src/components/product/ProductInfoTitle";
+import ProductInfoPrice from "../../src/components/product/ProductInfoPrice";
 
 export default function Product() {
   return (
@@ -9,6 +10,7 @@ export default function Product() {
       <Header />
       <ProductImage url="/images/nft.jpg" />
       <ProductInfoTitle text="Shallow Son" />
+      <ProductInfoPrice amount={"3"} currency={"ETH"} />
       <Footer />
     </>
   );
