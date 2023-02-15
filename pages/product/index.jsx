f96dee46d16976dde600b1ef3695e5a7f5c5b632
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-02-15 20:36:15 +0200

    Product Info Likes

--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -4,6 +4,7 @@ import ProductImage from "../../src/components/product/ProductImage";
 import ProductInfoTitle from "../../src/components/product/ProductInfoTitle";
 import ProductInfoPrice from "../../src/components/product/ProductInfoPrice";
 import ProductInfoStatus from "../../src/components/product/ProductInfoStatus";
+import ProductInfoLikes from "../../src/components/product/ProductInfoLikes";
 
 export default function Product() {
   return (
@@ -13,6 +14,7 @@ export default function Product() {
       <ProductInfoTitle text="Shallow Son" />
       <ProductInfoPrice amount={"3"} currency={"ETH"} />
       <ProductInfoStatus />
+      <ProductInfoLikes amount={100000} />
       <Footer />
     </>
   );
