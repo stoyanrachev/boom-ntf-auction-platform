a6457941f117c01e18fd266aed7ac138950313f5
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-02-19 18:19:15 +0200

    tabs 10

--- src/components/product/ProductTabs.jsx
+++ src/components/product/ProductTabs.jsx
@@ -6,7 +6,47 @@ import { parseISO, formatDistance } from "date-fns";
 
 import { useState } from "react";
 
-export default function ProductTabs({ text, bids = [] }) {
+export default function ProductTabs({
+  text = "Beaten back with a crack not knowing what was two and frow. The education system spews out to the TAB the broken promises. Unable to find a journey in a sea of dead bolted doors behind their salvation.",
+  bids = [
+    {
+      user: {
+        avatar: "/images/avatar.png",
+        name: "Cupcat NFT 1",
+        verified: true,
+      },
+      amount: 300,
+      date: "2023-03-22T05:29:23.382Z",
+    },
+    {
+      user: {
+        avatar: "/images/avatar.png",
+        name: "Cupcat NFT 2",
+        verified: true,
+      },
+      amount: 1050,
+      date: "2023-03-22T06:29:23.382Z",
+    },
+    {
+      user: {
+        avatar: "/images/avatar.png",
+        name: "Cupcat NFT 3",
+        verified: true,
+      },
+      amount: 530,
+      date: "2023-03-22T08:29:23.382Z",
+    },
+    {
+      user: {
+        avatar: "/images/avatar.png",
+        name: "Cupcat NFT 4",
+        verified: true,
+      },
+      amount: 15000,
+      date: "2023-03-22T09:29:23.382Z",
+    },
+  ],
+}) {
   const [value, setValue] = useState("1");
   const handleTabChange = (event, newValue) => setValue(newValue);
 
@@ -23,7 +63,31 @@ export default function ProductTabs({ text, bids = [] }) {
           <Tab label={"Bids"} value="2" className={styles["tab-bids"]} />
         </TabList>
 
-
+        <TabPanel value="1" className={styles["text-details"]}>
+          {text}
+        </TabPanel>
+        <TabPanel value="2" className={styles["bids-view"]}>
+          <Table area-label={"product details table"}>
+            <TableBody>
+              {bids.map((bid, i) => (
+                <TableRow
+                  className={`table-row-${i} ${styles["table-row"]}`}
+                  key={i}
+                >
+                  <TableCell sx={{ borderBottom: "none" }}>
+                    <User {...bid.user} size={34} />
+                  </TableCell>
+                  <TableCell className={styles["bid-cell"]}>
+                    {bid.amount}
+                  </TableCell>
+                  <TableCell className={styles.cell}>
+                    {formatDistance(parseISO(bid.date), Date.now())}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TabPanel>
       </TabContext>
     </div>
   );
