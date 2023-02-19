e859d4b72bc0e591ca997290d1c967ee27ba7ceb
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-02-19 10:11:17 +0200

    Creator Component

--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -5,6 +5,7 @@ import ProductInfoTitle from "../../src/components/product/ProductInfoTitle";
 import ProductInfoPrice from "../../src/components/product/ProductInfoPrice";
 import ProductInfoStatus from "../../src/components/product/ProductInfoStatus";
 import ProductInfoLikes from "../../src/components/product/ProductInfoLikes";
+import ProductInfoCreator from "../../src/components/product/ProductInfoCreator";
 
 export default function Product() {
   return (
@@ -15,6 +16,7 @@ export default function Product() {
       <ProductInfoPrice amount={"3"} currency={"ETH"} />
       <ProductInfoStatus />
       <ProductInfoLikes amount={100000} />
+      <ProductInfoCreator name={"Creator"} avatar={"/images/avatar.png"} verified={true} />
       <Footer />
     </>
   );
