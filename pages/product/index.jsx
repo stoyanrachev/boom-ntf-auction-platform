f134e5acf6316df009e4b6afb03ba58a8781f216
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-02-19 10:50:14 +0200

    Info component

--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -7,6 +7,7 @@ import ProductInfoStatus from "../../src/components/product/ProductInfoStatus";
 import ProductInfoLikes from "../../src/components/product/ProductInfoLikes";
 import ProductInfoCreator from "../../src/components/product/ProductInfoCreator";
 import ProductInfoTimer from "../../src/components/product/ProductInfoTimer";
+import ProductInfo from "../../src/components/product/ProductInfo";
 
 export default function Product() {
   return (
@@ -17,8 +18,26 @@ export default function Product() {
       <ProductInfoPrice amount={"3"} currency={"ETH"} />
       <ProductInfoStatus />
       <ProductInfoLikes amount={100000} />
-      <ProductInfoCreator name={"Creator"} avatar={"/images/avatar.png"} verified={true} />
+      <ProductInfoCreator
+        name={"Creator"}
+        avatar={"/images/avatar.png"}
+        verified={true}
+      />
       <ProductInfoTimer timeEnd={"2023-12-15T11:02:08.754Z"} />
+      <ProductInfo
+        title={"name"}
+        creator={{
+          name: "creator name",
+          avatar: "/images/avatar.png",
+          verified: true,
+        }}
+        price={10}
+        currency={"ETH"}
+        likes={1000}
+        timeEnd={"2023-12-15T11:02:08.754Z"}
+        onTimeEnd={null}
+        isLive={true}
+      />
       <Footer />
     </>
   );
