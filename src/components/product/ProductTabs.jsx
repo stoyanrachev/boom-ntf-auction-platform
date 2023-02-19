8f8645497bb857e08da34a31946247959f0cfa07
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-02-19 16:36:59 +0200

    tabs 6

--- src/components/product/ProductTabs.jsx
+++ src/components/product/ProductTabs.jsx
@@ -1,54 +1,80 @@
+import classNames from "classnames";
 import styles from "./ProductTabs.module.scss";
+import { TabContext } from "@mui/lab";
 import User from "../user/User";
-import { Tab, TableBody, Table, TableRow, TableCell } from "@mui/material";
-import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { parseISO, formatDistance } from "date-fns";
-
+import { TableRow, Box, Tabs, Tab, TableCell } from "@mui/material";
 import { useState } from "react";
+import formatDistance from "date-fns/formatDistance";
+import parseISO from "date-fns/parseISO";
 
-export default function ProductTabs({ text, bids = [] }) {
-  const [value, setValue] = useState("1");
-  const handleTabChange = (event, newValue) => setValue(newValue);
+export default function ProductTabs({
+  text,
+  bids = [
+    {
+      user: { avatar: "String", name: "Jonny", verified: true },
+      amount: "4 ETH",
+      date: formatDistance(parseISO("2022-02-06T22:18:15"), new Date(), {
+        includeSeconds: true,
+      }),
+    },
+    {
+      user: { avatar: "String", name: "Ronny", verified: true },
+      amount: "1 ETH",
+      date: formatDistance(parseISO("2021-08-11T11:30:30"), new Date(), {
+        includeSeconds: true,
+      }),
+    },
+  ],
+}) {
+  const [selectedTab, setSelectedTab] = useState();
+
+  const handleChange = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
 
   return (
-    <div className={styles["product-tabs"]}>
-      <TabContext value={value}>
-        <TabList
-          value={value}
-          onChange={handleTabChange}
-          area-label={"product tabs"}
-          className={styles["tab-buttons"]}
-        >
-          <Tab label={"Details"} value="1" className={styles["tab-details"]} />
-          <Tab label={"Bids"} value="2" className={styles["tab-bids"]} />
-        </TabList>
-
-        <TabPanel value="1" className={styles["text-details"]}>
-          {text}
-        </TabPanel>
-        <TabPanel value="2" className={styles["bids-view"]}>
-          <Table area-label={"product details table"}>
-            <TableBody>
-              {bids.map((bid, i) => (
-                <TableRow
-                  className={`table-row-${i} ${styles["table-row"]}`}
-                  key={i}
-                >
-                  <TableCell sx={{ borderBottom: "none" }}>
-                    <User {...bid.user} size={34} />
-                  </TableCell>
-                  <TableCell className={styles["bid-cell"]}>
-                    {bid.amount}
-                  </TableCell>
-                  <TableCell className={styles.cell}>
-                    {formatDistance(parseISO(bid.date), Date.now())}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TabPanel>
+    <div className={classNames(styles["product-tabs"])}>
+      <TabContext value="0">
+        <Box sx={{ borderBottom: 3 }}>
+          <Tabs value={selectedTab} onChange={handleChange}>
+            <Tab
+              label="Details"
+              className={classNames(styles["tab-details"])}
+            ></Tab>
+            <Tab label="Bids" className={classNames(styles["tab-bids"])}></Tab>
+          </Tabs>
+        </Box>
       </TabContext>
+      {selectedTab === 0 && <p className={classNames(styles.text)}>{text}</p>}
+      {selectedTab === 1 && (
+        <table className={classNames(styles.table)}>
+          {bids.map((bid, i) => (
+            <TableRow className={`table-row-${i}`}>
+              <div
+                className={
+                  i % 2 !== 0
+                    ? classNames(styles.light)
+                    : classNames(styles.dark)
+                }
+              >
+                <TableCell className={classNames(styles.name)}>
+                  <User name={bid.user.name}></User>
+                </TableCell>
+                <TableCell>
+                  <span className={classNames(styles.amount)}>
+                    {bid.amount}
+                  </span>
+                </TableCell>
+                <TableCell>
+                  <span className={classNames(styles.date)}>
+                    {bid.date} ago
+                  </span>
+                </TableCell>
+              </div>
+            </TableRow>
+          ))}
+        </table>
+      )}
     </div>
   );
 }
