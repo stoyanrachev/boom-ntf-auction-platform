afddb4a6e33ac7f7b0824179a30b87c0aeb9d1a7
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-02-19 10:36:36 +0200

    fix component render

--- src/components/product/ProductInfoTimer.jsx
+++ src/components/product/ProductInfoTimer.jsx
@@ -1,24 +1,31 @@
-import styles from "./ProductInfoTimer.module.scss"
-import Countdown from 'react-countdown';
+import styles from "./ProductInfoTimer.module.scss";
+import Countdown from "react-countdown";
 
-export default function ProductInfoTimer({timeEnd=null, onTimeEnd}){
-    return (
-        <div 
-            className={`${styles["product-info-timer"]} ${timeEnd && styles.active}`}>
-            <div className={styles.header}>
-                <p className={styles.title}>Ends in</p>
-            </div>
-            <div className={styles.timer}>
-                { timeEnd  && (
-                <Countdown 
-                    onComplete={onTimeEnd}
-                    date={timeEnd}
-                    renderer = {({ hours, minutes, seconds }) => (
-                        <span suppressHydrationWarning={true}>{hours}:{minutes}:{seconds}</span>
-                    )}
-                    />
-                )}
-            </div>
+export default function ProductInfoTimer({ timeEnd = null, onTimeEnd }) {
+  return (
+    <>
+      {timeEnd && (
+        <div
+          className={`${styles["product-info-timer"]} ${
+            timeEnd && styles.active
+          }`}
+        >
+          <div className={styles.header}>
+            <p className={styles.title}>Ends in</p>
+          </div>
+          <div className={styles.timer}>
+            <Countdown
+              onComplete={onTimeEnd}
+              date={timeEnd}
+              renderer={({ hours, minutes, seconds }) => (
+                <span suppressHydrationWarning={true}>
+                  {hours}:{minutes}:{seconds}
+                </span>
+              )}
+            />
+          </div>
         </div>
-    )
+      )}
+    </>
+  );
 }
