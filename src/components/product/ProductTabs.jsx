e8788d7fedf16839f3b14184a6010278ecfda85b
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-02-20 23:39:44 +0200

    tabs 12

--- src/components/product/ProductTabs.jsx
+++ src/components/product/ProductTabs.jsx
@@ -1,88 +1,58 @@
 import styles from "./ProductTabs.module.scss";
 import User from "../user/User";
-import { Tab, TableBody, Table, TableRow, TableCell } from "@mui/material";
-import TabContext from '@mui/lab/TabContext';
-import {  TabList, TabPanel } from "@mui/lab";
-import { parseISO, formatDistance } from "date-fns";
-
 import { useState } from "react";
+import classNames from "classnames";
+import { formatDistance, parseISO } from "date-fns";
+import { Tab, Table, TableBody, TableRow, TableCell } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
 
-export default function ProductTabs({
-  text = "Beaten back with a crack not knowing what was two and frow. The education system spews out to the TAB the broken promises. Unable to find a journey in a sea of dead bolted doors behind their salvation.",
-  bids = [
-    {
-      user: {
-        avatar: "/images/avatar.png",
-        name: "Cupcat NFT 1",
-        verified: true,
-      },
-      amount: 300,
-      date: "2023-03-22T05:29:23.382Z",
-    },
-    {
-      user: {
-        avatar: "/images/avatar.png",
-        name: "Cupcat NFT 2",
-        verified: true,
-      },
-      amount: 1050,
-      date: "2023-03-22T06:29:23.382Z",
-    },
-    {
-      user: {
-        avatar: "/images/avatar.png",
-        name: "Cupcat NFT 3",
-        verified: true,
-      },
-      amount: 530,
-      date: "2023-03-22T08:29:23.382Z",
-    },
-    {
-      user: {
-        avatar: "/images/avatar.png",
-        name: "Cupcat NFT 4",
-        verified: true,
-      },
-      amount: 15000,
-      date: "2023-03-22T09:29:23.382Z",
-    },
-  ],
-}) {
+export default function ProductTabs({ text = " ", bids = [] }) {
   const [value, setValue] = useState("1");
-  const handleTabChange = (event, newValue) => setValue(newValue);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const setOpacity = (index) => {
+    if (index % 2 == 0) {
+      return "rgba(78,36,242, 0.05)";
+    }
+    return "rgba(78,36,242, 0.15)";
+  };
 
   return (
     <div className={styles["product-tabs"]}>
       <TabContext value={value}>
-        <TabList
-          value={value}
-          onChange={handleTabChange}
-          area-label={"product tabs"}
-          className={styles["tab-buttons"]}
+        <div
+          className={styles.tabs}
+          sx={{ borderBottom: 1, borderColor: "divider" }}
         >
-          <Tab label={"Details"} value="1" className={styles["tab-details"]} />
-          <Tab label={"Bids"} value="2" className={styles["tab-bids"]} />
-        </TabList>
-
-        <TabPanel value="1" className={styles["text-details"]}>
-          {text}
-        </TabPanel>
-        <TabPanel value="2" className={styles["bids-view"]}>
-          <Table area-label={"product details table"}>
+          <TabList onChange={handleChange}>
+            <Tab className={styles["tab-details"]} label="DETAILS" value="1" />
+            <Tab className={styles["tab-bids"]} label="BIDS" value="2" />
+          </TabList>
+        </div>
+        <TabPanel value="1">{text}</TabPanel>
+        <TabPanel value="2">
+          <Table>
             <TableBody>
-              {bids.map((bid, i) => (
+              {bids.map((bid, index) => (
                 <TableRow
-                  className={`table-row-${i} ${styles["table-row"]}`}
-                  key={i}
+                  className={classNames(
+                    styles["table-row"],
+                    `table-row-${index}`
+                  )}
+                  key={index}
+                  sx={{ backgroundColor: setOpacity(index) }}
                 >
-                  <TableCell sx={{ borderBottom: "none" }}>
-                    <User {...bid.user} size={34} />
+                  <TableCell>
+                    <User {...bid.user} />
                   </TableCell>
-                  <TableCell className={styles["bid-cell"]}>
-                    {bid.amount}
-                  </TableCell>
-                  <TableCell className={styles.cell}>
-                    {formatDistance(parseISO(bid.date), Date.now())}
+                  <TableCell>{bid.amount} ETH</TableCell>
+                  <TableCell>
+                    {formatDistance(parseISO(bid.date), new Date(), {
+                      addSuffix: true,
+                    })}
                   </TableCell>
                 </TableRow>
               ))}
