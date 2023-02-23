ecf239409dd6c5455e3f4900f72be2cf9b24983f
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-02-23 21:19:14 +0200

    Product Container 3

--- src/components/product/ProductContainer.jsx
+++ src/components/product/ProductContainer.jsx
@@ -1,39 +1,62 @@
 import styles from "./ProductContainer.module.scss";
-import classNames from "classnames";
-import { Container ,Grid } from "@mui/material";
 import ProductImage from "./ProductImage";
 import ProductInfo from "./ProductInfo";
 import ProductTabs from "./ProductTabs";
 import ProductActions from "./ProductActions";
+import { Grid, Container } from "@mui/material";
+import classNames from "classnames";
 
-
-
-export default function ProductContainer({ name="", owner, price, currency, likes, auction_end, details, source, bids}) {
-
-    return (
-        <div className={classNames(styles["product-container"])}>
-            <Container>
-              <Grid container justifyContent={"space-around"}>
-                  <Grid item xs={6}>
-                      <ProductImage url={source.url}/>
-                  </Grid>
-                  <Grid item xs={5}>
-                      <ProductInfo 
-                      title={name} 
-                      creator={{name:owner.username, avatar:owner.avatar.url, verified:owner.verified, id:owner.id}}
-                      price={price}
-                      currency={currency}
-                      likes={likes}
-                      timeEnd={auction_end}
-                      />
-                      <ProductTabs text={details} bids={bids}/>
-                      <ProductActions 
-                      currency={currency} 
-                      buyAmount={price} 
-                      />
-                  </Grid>
-              </Grid>
-            </Container>
-        </div>
-    )
+export default function ProductContainer({
+  name,
+  owner,
+  price,
+  currency,
+  likes,
+  auction_end,
+  details,
+  bids,
+  source,
+  isLive,
+  buyAmount,
+  bidAmount,
+  onBuy,
+  onBid,
+  onTimeEnd,
+}) {
+  return (
+    <div className={classNames(styles["product-container"])}>
+      <Container maxWidth="xl">
+        <Grid container spacing={8}>
+          <Grid item xs={6}>
+            <ProductImage url={source.url} />
+          </Grid>
+          <Grid item xs={5}>
+            <ProductInfo
+              title={name}
+              creator={{
+                name: owner.username,
+                avatar: owner.avatar.url,
+                verified: owner.verified,
+              }}
+              price={price}
+              currency={currency}
+              likes={likes}
+              timeEnd={auction_end}
+              onTimeEnd={onTimeEnd}
+              isLive={isLive}
+            />
+            <ProductTabs text={details} bids={bids} />
+            <ProductActions
+              isLive={isLive}
+              buyAmount={buyAmount}
+              bidAmount={bidAmount}
+              currency={currency}
+              onBuy={onBuy}
+              onBid={onBid}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
