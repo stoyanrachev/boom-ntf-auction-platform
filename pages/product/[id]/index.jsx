a9cd7c0d3ef5e88f8f1ade02829a20a8b2a5bdde
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-03-05 22:39:24 +0200

    check 2

--- pages/product/[id]/index.jsx
+++ pages/product/[id]/index.jsx
@@ -17,64 +17,62 @@ export default function Product() {
   return (
     <>
       <Header />
-      {dataNft && (
-        <ProductContainer
-          name={dataNft.name}
-          owner={dataNft.owner}
-          price={dataNft.price}
-          currency={dataNft.currency}
-          likes={dataNft.likes}
-          auction_end={dataNft.auction_end}
-          details={
-            "Beaten back with a crack not knowing what was two and frow. The education system spews out to the TAB the broken promises. Unable to find a journey in a sea of dead bolted doors behind their salvation."
-          }
-          bids={[
-            {
-              user: {
-                avatar: "/images/avatar.png",
-                name: "Cupcat NFT 1",
-                verified: true,
-              },
-              amount: 300,
-              date: "2023-03-22T05:29:23.382Z",
+      <ProductContainer
+        name={dataNft.name}
+        owner={dataNft.owner}
+        price={dataNft.price}
+        currency={dataNft.currency}
+        likes={dataNft.likes}
+        auction_end={dataNft.auction_end}
+        details={
+          "Beaten back with a crack not knowing what was two and frow. The education system spews out to the TAB the broken promises. Unable to find a journey in a sea of dead bolted doors behind their salvation."
+        }
+        bids={[
+          {
+            user: {
+              avatar: "/images/avatar.png",
+              name: "Cupcat NFT 1",
+              verified: true,
             },
-            {
-              user: {
-                avatar: "/images/avatar.png",
-                name: "Cupcat NFT 2",
-                verified: true,
-              },
-              amount: 1050,
-              date: "2023-03-22T06:29:23.382Z",
+            amount: 300,
+            date: "2023-03-22T05:29:23.382Z",
+          },
+          {
+            user: {
+              avatar: "/images/avatar.png",
+              name: "Cupcat NFT 2",
+              verified: true,
             },
-            {
-              user: {
-                avatar: "/images/avatar.png",
-                name: "Cupcat NFT 3",
-                verified: true,
-              },
-              amount: 530,
-              date: "2023-03-22T08:29:23.382Z",
+            amount: 1050,
+            date: "2023-03-22T06:29:23.382Z",
+          },
+          {
+            user: {
+              avatar: "/images/avatar.png",
+              name: "Cupcat NFT 3",
+              verified: true,
             },
-            {
-              user: {
-                avatar: "/images/avatar.png",
-                name: "Cupcat NFT 4",
-                verified: true,
-              },
-              amount: 15000,
-              date: "2023-03-22T09:29:23.382Z",
+            amount: 530,
+            date: "2023-03-22T08:29:23.382Z",
+          },
+          {
+            user: {
+              avatar: "/images/avatar.png",
+              name: "Cupcat NFT 4",
+              verified: true,
             },
-          ]}
-          source={dataNft.source}
-          isLive={true}
-          buyAmount={3}
-          bidAmount={1}
-          onBuy={() => {}}
-          onBid={() => {}}
-          onTimeEnd={() => {}}
-        />
-      )}
+            amount: 15000,
+            date: "2023-03-22T09:29:23.382Z",
+          },
+        ]}
+        source={dataNft.source}
+        isLive={true}
+        buyAmount={3}
+        bidAmount={1}
+        onBuy={() => {}}
+        onBid={() => {}}
+        onTimeEnd={() => {}}
+      />
       <Footer />
     </>
   );
