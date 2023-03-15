3f0d7c7e7d3f68942df329d4abebfd85cdae3b6b
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-03-15 23:00:35 +0200

    Try to fix ExploreFilters 3

--- src/components/explore/ExploreFilters.jsx
+++ src/components/explore/ExploreFilters.jsx
@@ -12,69 +12,6 @@ import {
 import SearchIcon from "@mui/icons-material/Search";
 import { useState } from "react";
 
-
-function ExploreFilters_OLD({ filters }) {
-  const [sortBy, setSortBy] = useState("");
-  const [priceRange, setPriceRange] = useState("");
-
-  return (
-    <div className={styles["explore-filters"]}>
-      <Stack direction={"row"} spacing={2} justifyContent={"flex-end"}>
-        <FormControl>
-          <InputLabel id="sort-by-label">Sort by</InputLabel>
-          <Select
-            labelId="sort-by-label"
-            value={sortBy}
-            sx={{ width: "220px" }}
-            color={"primary"}
-            onChange={(e) => setSortBy(e.target.value)}
-          >
-            {filters.sort.map((filter) => {
-              return (
-                <MenuItem key={filter.label} value={filter.value}>
-                  {filter.label}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-        <FormControl>
-          <InputLabel id="price-range-label">Price range</InputLabel>
-          <Select
-            labelId="price-range-label"
-            value={priceRange}
-            sx={{ minWidth: "220px" }}
-            color={"primary"}
-            onChange={(e) => setPriceRange(e.target.value)}
-          >
-            {filters.price.map((filter) => {
-              return (
-                <MenuItem key={filter.label} value={filter.value}>
-                  {filter.label}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-        <TextField
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon className={styles["search-icon"]} />
-              </InputAdornment>
-            ),
-            className: styles.search,
-            disableUnderline: true,
-            sx: { width: "340px" },
-          }}
-          variant="standard"
-        />
-      </Stack>
-    </div>
-  );
-}
-
-
 export default function ExploreFilters({
   filters,
   sortValue,
